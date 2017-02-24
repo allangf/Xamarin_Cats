dca32bdfc8c16a0a0646fc60b9b06e1b9985388a
Allan G. Fernandes <all_ghosts@yahoo.com.br>
2017-02-24 13:46:03 -0300

    Upload Projeto Cats

---
+++ Cats/Cats/Models/Repository.cs
@@ -0,0 +1,24 @@
+﻿using Cats.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cats
+{
+    class Repository
+    {
+        List<Cat> Cats;
+        public async Task<List<Cat>> GetCats()
+        {
+            var URLWebAPI = "http://demos.ticapacitacion.com/cats";
+            using(var Client = new System.Net.Http.HttpClient())
+            {
+                var JSON = await Client.GetStringAsync(URLWebAPI);
+                Cats = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Cat>>(JSON);
+            }
+            return Cats;
+        }
+    }
+}
---
+++ Cats/Cats/View/DetailsPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cats.View
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailsPage : ContentPage
+    {
+        Models.Cat SelectedCat;
+
+        public DetailsPage(Models.Cat selectedCat)
+        {
+            InitializeComponent();
+            this.SelectedCat = selectedCat;
+            BindingContext = this.SelectedCat;
+            ButtonWebSite.Clicked += ButtonWebSite_Clicked;
+        }
+
+        private void ButtonWebSite_Clicked(object sender, EventArgs e)
+        {
+            if (SelectedCat.WebSite.StartsWith("http"))
+            {
+                Device.OpenUri(new Uri(SelectedCat.WebSite));
+            }
+        }
+    }
+}
